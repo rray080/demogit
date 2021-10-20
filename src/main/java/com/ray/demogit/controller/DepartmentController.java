c33fde9020303737f780e535c67a69e6455c2b26
Rakesh Kymar Ray <rakeshrayk@gmail.com>
2021-10-20 15:09:38 +0530

    Update DepartmentController.java

--- src/main/java/com/ray/demogit/controller/DepartmentController.java
+++ src/main/java/com/ray/demogit/controller/DepartmentController.java
@@ -1,10 +1,18 @@
 package com.ray.demogit.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ray.demogit.model.Department;
 
 @RestController
 @RequestMapping("/api/departments")
 public class DepartmentController {
-
+	// adding get mapping for department
+	@GetMapping("/department/{id}")
+	public Department getDepartment(@PathVariable long id) {
+		return new Department();
+	}
 }
