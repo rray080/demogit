b4b77cce4be8c5f8de41810bd9403c0f8a54dfe7
rakeshraycutm <rakesh.ray@cutm.ac.in>
2021-10-20 10:00:59 +0530

    Update Employee.java

--- src/main/java/com/ray/demogit/model/Employee.java
+++ src/main/java/com/ray/demogit/model/Employee.java
@@ -4,4 +4,27 @@ public class Employee {
 
 	private long id;
 	private String name;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Employee(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
