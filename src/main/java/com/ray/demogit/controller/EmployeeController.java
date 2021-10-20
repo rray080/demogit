1ef33a87ce6cf5692a579f9d10e7721ce6cbaef1
rakeshraycutm <rakesh.ray@cutm.ac.in>
2021-10-20 09:38:14 +0530

    Update EmployeeController.java

--- src/main/java/com/ray/demogit/controller/EmployeeController.java
+++ src/main/java/com/ray/demogit/controller/EmployeeController.java
@@ -1,10 +1,19 @@
 package com.ray.demogit.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ray.demogit.model.Employee;
 
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
-
+	
+	// added a post request
+	@PostMapping("/employee")
+	public Employee addEmp(@RequestBody Employee emp) {
+		return emp;
+	}
 }
