667237e95b51db277efc27377b6c586beab9f541
Rakesh Kymar Ray <rakeshrayk@gmail.com>
2021-10-20 15:39:14 +0530

    Update Department.java

--- src/main/java/com/ray/demogit/model/Department.java
+++ src/main/java/com/ray/demogit/model/Department.java
@@ -5,4 +5,25 @@ public class Department {
 	private long id;
 	private String name;
 	private String desc;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	
 }
